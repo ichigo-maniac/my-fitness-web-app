dc7f73d40ffcd95ea1dcb1810e3e962546f2cf53
impi <ichigomaniac@gmail.com>
2018-02-11 09:45:15 +0300

    first commit

---
+++ src/main/java/com/my/fitness/MyFitnessWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.my.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitnessWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessWebAppApplication.class, args);
+	}
+}
