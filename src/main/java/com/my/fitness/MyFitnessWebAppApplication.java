3dece3d9128da6611fa47a41cf33613654bcb5a1
Dmitry Konoplev <dmitry.konoplev@citeck.ru>
2018-02-15 18:54:45 +0300

    Added jsp templates configuration

--- src/main/java/com/my/fitness/MyFitnessWebAppApplication.java
+++ src/main/java/com/my/fitness/MyFitnessWebAppApplication.java
@@ -2,10 +2,28 @@ package com.my.fitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * My fitness web application
+ */
 @SpringBootApplication
-public class MyFitnessWebAppApplication {
+public class MyFitnessWebAppApplication extends SpringBootServletInitializer {
 
+	/**
+	 * Configure application
+	 * @param application Application
+	 * @return Application builder
+	 */
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyFitnessWebAppApplication.class);
+	}
+
+	/**
+	 * Main function
+	 * @param args Arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MyFitnessWebAppApplication.class, args);
 	}
